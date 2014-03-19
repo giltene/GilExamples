28d3c72e412a8a16fb309e72babb847c27f49a1b
Gil Tene <gil@cloud7.com>
2014-03-19 09:16:19 -0700

    Fix more

--- src/main/java/InlineCacheExample.java
+++ src/main/java/InlineCacheExample.java
@@ -39,16 +39,39 @@ public class InlineCacheExample {
         return sum;
     }
 
+    static Long compareToLong = new Long(0xffff);
+
+    static long playWithEqualsA(Object thing) {
+        long sum = 0;
+        for (int i = 0; i < 1000000; i++) {
+            sum += thing.equals(compareToLong) ? 1 : 0;
+        }
+        return sum;
+    }
+
+    static long playWithEqualsB(Object thing) {
+        long sum = 0;
+        for (int i = 0; i < 1000000; i++) {
+            sum += thing.equals(compareToLong) ? 1 : 0;
+        }
+        return sum;
+    }
+
+
+
     public static long doIt(int color) {
         long sum = 0;
         ClassWithColor thingOneWithColor = new ClassWithColor(color);
         ClassWithColor thingTwoWithColor = new SubClassWithColor(color);
+        Long colorLong = new Long(color);
 
-        // playWithColorA is monomorphic:
+        // playWithColorA and playWithEqualsA are monomorphic:
         sum += playWithColorA(thingOneWithColor);
+        sum += playWithEqualsA(colorLong);
 
-        // playWithColorB is megamorphic:
+        // playWithColorB and playWithEqualsB are megamorphic:
         sum += playWithColorB(((color & 1) == 1) ? thingOneWithColor : thingTwoWithColor);
+        sum += playWithEqualsB(((color & 1) == 1) ? colorLong : thingTwoWithColor);
 
         return sum;
     }
