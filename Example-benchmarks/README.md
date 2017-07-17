b95c3ad9db31602414ef79f06dd93775795f52b6
Gil Tene <gil@cloud7.com>
2017-07-17 15:52:28 -0700

    Adds Example-benchmarks

---
+++ Example-benchmarks/README.md
@@ -0,0 +1,7 @@
+This is a small set of microbennchmark that can be used demonstrate stuff
+
+To execute:
+```
+mvn clean package
+${JAVA_HOME}/bin/java -jar Example-benchmarks/target/benchmarks.jar
+```
---
+++ Example-benchmarks/src/main/java/bench/ExampleBench.java
@@ -0,0 +1,96 @@
+/**
+ * Written by Gil Tene of Azul Systems, and released to the public domain,
+ * as explained at http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * @author Gil Tene
+ */
+
+package bench;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+  Run all benchmarks:
+    $ java -jar target/benchmarks.jar
+
+  Run selected benchmarks:
+    $ java -jar target/benchmarks.jar (regexp)
+
+  Run the profiling (Linux only):
+     $ java -Djmh.perfasm.events=cycles,cache-misses -jar target/benchmarks.jar -f 1 -prof perfasm
+ */
+@Warmup(iterations = 7, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@State(Scope.Thread)
+
+public class ExampleBench {
+
+
+    @Param({"4096"})
+    int bufferLength;
+
+    static volatile long baseVal = 0;
+
+    long sum = 0;
+
+    static class MyBuffer {
+        final byte[] buf;
+        MyBuffer(int length) {
+            this.buf = new byte[length];
+        }
+
+        long sumBuf1() {
+            long sum = 0;
+            for (int i = 0; i < buf.length; i++) {
+                sum += buf[i] + baseVal;
+            }
+            return sum;
+        }
+
+        long sumBuf2() {
+            long sum = 0;
+            for (byte b: buf) {
+                sum += b + baseVal;
+            }
+            return sum;
+        }
+
+        long sumBuf3() {
+            long sum = 0;
+            byte[] localBuf = buf;
+            for (int i = 0; i < localBuf.length; i++) {
+                sum += localBuf[i] + baseVal;
+            }
+            return sum;
+        }
+    }
+
+    MyBuffer buffer;
+
+    @Setup
+    public void setup() throws NoSuchMethodException {
+        buffer = new MyBuffer(bufferLength);
+
+        for (int i = 0; i < bufferLength; i++) {
+            buffer.buf[i] = (byte)(i % 31);
+        }
+    }
+
+    @Benchmark
+    public void sum1() {
+        sum += buffer.sumBuf1();
+    }
+
+    @Benchmark
+    public void sum2() {
+        sum += buffer.sumBuf2();
+    }
+
+    @Benchmark
+    public void sum3() {
+        sum += buffer.sumBuf3();
+    }
+}
